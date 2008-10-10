19d49056fe9d19f1443bb3f80c78bab1507beaee
mh <mh@immerda.ch>
2008-10-10 11:22:02 +0000

    don't restart network directly anymore, rather notify the admin, that something has changed, so he can take care of manually. As in the past there have been weird problems due to automatic network restarts by /etc/netstart

--- manifests/network.pp
+++ manifests/network.pp
@@ -1,8 +1,12 @@
 # manifests/network.pp
 
 class openbsd::network {
+    # we only notify the root
+    # to restart the box, as this
+    # is more safe than to directly 
+    # restart the network
     exec{'restart_network':
-        command => '/bin/sh /etc/netstart',
+        command => "echo 'This host should be restarted as its network config changed' | mail -s 'Network change: restart ${fqdn}'"
         refreshonly => true,
     }
 }
