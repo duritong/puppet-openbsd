78324def8bbbc4fe153fe0234da5ab5e468da025
mh <mh@immerda.ch>
2010-10-16 17:46:44 +0200

    code style

--- manifests/router.pp
+++ manifests/router.pp
@@ -2,6 +2,10 @@
 
 # settings needed for a router
 class openbsd::router {
-    sysctl::value{"net.inet.ip.forwarding": value => "1"}
-    sysctl::value{"net.inet6.ip6.forwarding": value => "1"}
+  sysctl::value{
+    'net.inet.ip.forwarding':
+      value => 1;
+    'net.inet6.ip6.forwarding':
+      value => 1;
+  }
 }
