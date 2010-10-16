295ceb3ccb54a79a4214c7eb44fa88bbcdb6b768
mh <mh@immerda.ch>
2010-10-16 17:44:10 +0200

    use new define name

--- manifests/router.pp
+++ manifests/router.pp
@@ -2,6 +2,6 @@
 
 # settings needed for a router
 class openbsd::router {
-    sysctl::set_value{"net.inet.ip.forwarding": value => "1"}
-    sysctl::set_value{"net.inet6.ip6.forwarding": value => "1"}
+    sysctl::value{"net.inet.ip.forwarding": value => "1"}
+    sysctl::value{"net.inet6.ip6.forwarding": value => "1"}
 }
